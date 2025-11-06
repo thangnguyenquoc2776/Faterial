22f0157eb4b104945227c563537c5b913ad20163
K <nhxau2001@gmail.com>
2025-11-06 17:27:56 +0700

    update animation coin

--- Classes/game/objects/Coin.cpp
+++ Classes/game/objects/Coin.cpp
@@ -6,9 +6,27 @@ Coin* Coin::create(){ auto p=new(std::nothrow) Coin(); if(p && p->init()){p->aut
 bool Coin::init(){
     if(!Entity::init()) return false;
     setTagEx(phys::Tag::COIN);
-    auto dn = DrawNode::create();
-    dn->drawSolidCircle(Vec2::ZERO, 10, 0, 24, Color4F(1,0.85f,0,1));
-    addChild(dn);
+
+    _sprite = Sprite::create("sprites/objects/coin/coin_1.png"); // frame đầu tiên
+    _sprite->setScale(1.5f); // tùy chỉnh kích cỡ hiển thị
+    addChild(_sprite, 1);
+
+    Vector<SpriteFrame*> frames;
+    auto firstSize = _sprite->getContentSize();
+    for (int i = 1; i <= 4; i++) { // giả sử bạn có coin_1.png -> coin_5.png
+        std::string framePath = StringUtils::format("sprites/objects/coin/coin_%d.png", i);
+        auto frame = SpriteFrame::create(framePath, Rect(0, 0, firstSize.width, firstSize.height));
+        frame->setOriginalSize(firstSize);
+        frames.pushBack(frame);
+    }
+
+    auto animation = Animation::createWithSpriteFrames(frames, 0.2f); // 0.4s mỗi frame
+    auto animate = Animate::create(animation);
+    _sprite->runAction(RepeatForever::create(animate));
+
+    // auto dn = DrawNode::create();
+    // dn->drawSolidCircle(Vec2::ZERO, 10, 0, 24, Color4F(1,0.85f,0,1));
+    // addChild(dn);
     auto body = PhysicsBody::createCircle(10);
     body->setDynamic(false);
     body->setCategoryBitmask(phys::CAT_ITEM);
--- Classes/game/objects/Coin.h
+++ Classes/game/objects/Coin.h
@@ -1,8 +1,12 @@
 // Coin.h
 #pragma once
 #include "game/Entity.h"
+#include "2d/CCSprite.h"
+#include "2d/CCAnimation.h"
+#include "2d/CCAnimationCache.h"
 class Coin : public Entity {
 public:
     static Coin* create();
     bool init() override;
+    cocos2d::Sprite* _sprite = nullptr;
 };
